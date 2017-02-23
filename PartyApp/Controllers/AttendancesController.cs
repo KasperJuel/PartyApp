a55b68bf6600f2a2f452b19ed6485ffa48c05403
Kasper Juel <kaped82@gmail.com>
2017-02-23 14:29:02 +0100

    Created Attendance Api.

---
+++ PartyApp/Controllers/AttendancesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNet.Identity;
+using PartyApp.Models;
+using System.Linq;
+using System.Web.Http;
+
+namespace PartyApp.Controllers.Api
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody]int partyId)
+        {
+            var userId = User.Identity.GetUserId();
+
+            if (_context.Attendances.Any(a => a.AttendeeId == userId && a.PartyId == partyId))
+                return BadRequest("Deltageren findes allerede.");
+
+            var attendance = new Attendance
+            {
+                PartyId = partyId,
+                AttendeeId = userId
+            };
+
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
