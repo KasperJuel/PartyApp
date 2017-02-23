6b836a4cbfddc72a4f2309f00f87dfdbf85f3a6a
Kasper Juel <kaped82@gmail.com>
2017-02-23 11:15:04 +0100

    Added Date Picker with jQuery UI to Create.

--- PartyApp/App_Start/BundleConfig.cs
+++ PartyApp/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace PartyApp
 {
@@ -14,6 +13,9 @@ namespace PartyApp
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -25,7 +27,8 @@ namespace PartyApp
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/themes/base/jquery-ui.css"));
         }
     }
 }
