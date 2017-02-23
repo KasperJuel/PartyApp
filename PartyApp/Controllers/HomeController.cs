4874e10b90740f5c6f65bda26413014120e56e4b
Kasper Juel <kaped82@gmail.com>
2017-02-23 13:30:43 +0100

    Printed out Parties on Home Index.

--- PartyApp/Controllers/HomeController.cs
+++ PartyApp/Controllers/HomeController.cs
@@ -1,30 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using PartyApp.Models;
+using System;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace PartyApp.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
-            return View();
-        }
+            var upcomingParties = _context.Parties
+                .Include(p => p.User)
+                .Where(p => p.DateTime > DateTime.Now);
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return View(upcomingParties);
         }
     }
 }
