ca69eb7d383c2e581dbc40a6db4a61acef4a1e8b
Kasper Juel <kaped82@gmail.com>
2017-02-23 10:22:53 +0100

    Added Form to Create Party.

---
+++ PartyApp/Controllers/PartiesController.cs
@@ -0,0 +1,27 @@
+﻿using PartyApp.Models;
+using PartyApp.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace PartyApp.Controllers
+{
+    public class PartiesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PartiesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Create()
+        {
+            var model = new PartyFormViewModel
+            {
+                PartyTypes = _context.PartyTypes.ToList()
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ PartyApp/ViewModels/PartyFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using PartyApp.Models;
+using System.Collections.Generic;
+
+namespace PartyApp.ViewModels
+{
+    public class PartyFormViewModel
+    {
+        public string Location { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int PartyType { get; set; }
+        public IEnumerable<PartyType> PartyTypes { get; set; }
+    }
+}
