2270a072938f52595f289dcb730aa5ac09c5bfe0
Kasper Juel <kaped82@gmail.com>
2017-02-27 16:34:29 +0100

    Created Notifications Api.

---
+++ PartyApp/Controllers/Api/NotificationsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNet.Identity;
+using PartyApp.Models;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Http;
+
+namespace PartyApp.Controllers.Api
+{
+    [Authorize]
+    public class NotificationsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public NotificationsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public IEnumerable<Notification> GetNewNotifications()
+        {
+            var userId = User.Identity.GetUserId();
+
+            var notifications = _context.UserNotifications
+                .Where(un => un.UserId == userId)
+                .Select(un => un.Notification)
+                .Include(n => n.Party.User)
+                .ToList();
+
+            return notifications;
+        }
+    }
+}
